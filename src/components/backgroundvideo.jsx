b8c8c1824e245c49c4197c42d9ed26bfbb2a25ff
Dhinesh R <70008535+dhinesh2112002@users.noreply.github.com>
2023-08-21 12:32:16 +0530

    VIdeo

---
+++ src/components/backgroundvideo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import video from "../assets/21.mp4";
+import vivanata from"../assets/VIVANATA.png";
+import i2023 from "../assets/2023.png" 
+const Bgvideo = () =>
+{
+    return (
+        <div className="bgContainer">
+            
+            <div className="overlay"></div>
+            <nav class="shift">
+    <ul>
+      <li><a href="#">ABOUT US</a></li>
+      <li><a href="#">EVENTS</a></li>
+      <li><a href="#">SIGN IN/SIGN UP</a></li>
+    </ul>
+  </nav>
+                <video src={video} autoPlay loop muted/>
+                <div className="container">
+                    
+                <img src={vivanata} id="logo" alt=""/>
+                <img src={i2023} id="year" alt="" />
+                </div>
+        </div>
+    )
+}
+export default Bgvideo;
